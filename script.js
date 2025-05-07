8f034fc7faef9296c56a1f21089460e5fff3a3f7
imahnn <132784747+imahnn@users.noreply.github.com>
2025-05-07 22:25:31 +0100

    Add sketch boxes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("This is the log and your javascript is successfully connected and active")
+const container = document.querySelector(".container");
+const numberOfBoxes = 16;
+container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 1fr)`;
+
+function createRow(){
+    for (i = 0; i < numberOfBoxes; i++){
+        let gridBox = document.createElement("div");
+        container.appendChild(gridBox);
+        gridBox.style.backgroundColor = "red";
+        gridBox.style.aspectRatio = "1/1";
+        gridBox.style.padding = "auto";
+        gridBox.style.border = "2px solid black";
+    }
+}
+
+
+for (let i = 0; i < numberOfBoxes; i++){
+    createRow();
+}
