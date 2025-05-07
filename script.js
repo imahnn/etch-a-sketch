6ecc0ce7dc7fbc69a338d9cfb1cc145ff61d6ed2
imahnn <132784747+imahnn@users.noreply.github.com>
2025-05-07 22:53:02 +0100

    Add hover effect

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const container = document.querySelector(".container");
-const numberOfBoxes = 16;
+const numberOfBoxes = 100;
 container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 1fr)`;
-
+let gridBox = document.createElement("div");
 function createRow(){
     for (i = 0; i < numberOfBoxes; i++){
         let gridBox = document.createElement("div");
+        gridBox.classList = "gridBox";
+        gridBox.addEventListener("mouseenter", e => {
+            gridBox.style.backgroundColor = "black";
+        });
         container.appendChild(gridBox);
-        gridBox.style.backgroundColor = "red";
-        gridBox.style.aspectRatio = "1/1";
-        gridBox.style.padding = "auto";
-        gridBox.style.border = "2px solid black";
     }
 }
-
 
 for (let i = 0; i < numberOfBoxes; i++){
     createRow();
